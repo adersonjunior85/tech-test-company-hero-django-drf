129ad572dd03f224dd4bb0dcdeb078ea90243e01
Aderson Januario <46459925+adersonjunior85@users.noreply.github.com>
2021-11-05 15:12:22 -0300

    clean code

--- apihero/models.py
+++ apihero/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
 class Company(models.Model):
-    cnpj = models.CharField(max_length=50)
+    cnpj = models.CharField(max_length=14)
     nome = models.CharField(max_length=50)
     cnpj = models.CharField(max_length=40)
-    email = models.CharField(max_length=50)
     site = models.CharField(max_length=50)
-    endereco = models.CharField(max_length=150)
-    telefone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=250)
+    telefone = models.CharField(max_length=11)
 
     def __str__(self):
         return self.cnpj
 
 class Employee(models.Model):
     name = models.CharField(max_length=50)
+    idade = models.IntegerField()
     cargo = models.CharField(max_length=50)
     cpf = models.CharField(max_length=11, unique=True)
     companies = models.ManyToManyField(Company, related_name='employees')
